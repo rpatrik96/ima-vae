b5dab3f800b7d922796a2b403dd94f6388cc84ad
Patrik Reizinger <prmedia17@gmail.com>
2021-12-09 15:43:32 +0100

    import fix

--- ima_vae/simulations.py
+++ ima_vae/simulations.py
@@ -1,5 +1,5 @@
 from ima_vae.args import parse_sim
-from runners.simulation_runner import run_ivae_exp
+from ima_vae.runners.simulation_runner import run_ivae_exp
 
 if __name__ == '__main__':
     args = parse_sim()
