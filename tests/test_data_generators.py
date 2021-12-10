89df3b88f07d0f77364ea6c538f1ce1aeea714db
Patrik Reizinger <prmedia17@gmail.com>
2021-12-10 12:55:31 +0100

    moebius test

--- tests/test_data_generators.py
+++ tests/test_data_generators.py
@@ -1,4 +1,24 @@
+import jax.numpy as jnp
+import numpy as np
+from jax import jacfwd
+from scipy.stats import ortho_group
+
+from ima.ima.mixing_functions import build_moebius_transform
 
 
-def test_moebius():
-    pass
+def test_moebius_orthogonality(args):
+    # generate moebius transform
+    alpha = 1.0
+    mixing_matrix = ortho_group.rvs(args.latent_dim)
+    a = np.random.rand(args.latent_dim)
+    b = np.zeros(args.latent_dim)
+    mixing_moebius, _ = build_moebius_transform(alpha, mixing_matrix, a, b)
+
+    # calculate jacobian
+    jacobian = jacfwd(mixing_moebius)(a / 2)
+
+    q, _ = jnp.linalg.qr(jacobian)
+
+    # as the jacobian should be column-orthogonal,
+    # the Q matrix of the QR-decomposition should have a |det| = 1
+    assert abs(abs(jnp.linalg.det(q)) - 1) < 1e-5
