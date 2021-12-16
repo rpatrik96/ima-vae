moebius transform fix and logging gradients + model

ima_vae/data/data_generators.py
@@ -4,7 +4,6 @@ from scipy.stats import ortho_group
 from sklearn.preprocessing import scale
 from torch.utils.data import Dataset
 
-from ima.ima.mixing_functions import build_moebius_transform
 from .utils import to_one_hot
 
 
@@ -106,12 +105,14 @@ def gen_data(Ncomp, Nlayer, Nsegment, NsegmentObs, orthog, seed, NonLin, source=
     np.random.seed(seed)
 
     if mobius:
-        moeb_params = np.load('moebius_transform_params.npy', allow_pickle=True).item()
+        from os.path import dirname, abspath, join
+        dir = join(dirname(dirname(dirname(abspath(__file__)))), "ima/out/cima_obj/2d/moeb/0_5/data/")
+        moeb_params = np.load(join(dir, 'moebius_transform_params.npy'), allow_pickle=True).item()
         alpha = 1.0
         mixing_matrix = ortho_group.rvs(Ncomp)
         a = np.array(moeb_params['a'])
         b = np.zeros(2)
-        mixing_moebius = build_moebius_transform(alpha, mixing_matrix, a, b)
+        mixing_moebius, _ = build_moebius_transform(alpha, mixing_matrix, a, b)
         observations = mixing_moebius(observations)
     else:
         for l in range(nlayers):
@@ -131,4 +132,37 @@ def gen_data(Ncomp, Nlayer, Nsegment, NsegmentObs, orthog, seed, NonLin, source=
     if one_hot_labels:
         labels = to_one_hot(labels)[0]
 
-    return observations.astype(np.float32), labels.astype(np.float32), sources.astype(np.float32)
+    return np.asarray(observations.astype(np.float32)), np.asarray(labels.astype(np.float32)), np.asarray(sources.astype(np.float32))
+import jax.numpy as jnp
+
+# taken from IMA repo
+def build_moebius_transform(alpha, A, a, b, epsilon=2):
+    '''
+    Implements Möbius transformations for D>=2, based on:
+    https://en.wikipedia.org/wiki/Liouville%27s_theorem_(conformal_mappings)
+
+    alpha: a scalar
+    A: an orthogonal matrix
+    a, b: vectors in \RR^D (dimension of the data)
+    '''
+    def mixing_moebius_transform(x):
+        if epsilon==2:
+            frac = jnp.sum((x-a)**2) #is this correct?
+            frac = frac**(-1)
+        else:
+            diff = jnp.abs(x-a)
+
+            frac = 1.0
+        return b + frac * alpha * (A @ (x - a).T).T
+
+    B = jnp.linalg.inv(A)
+
+    def unmixing_moebius_transform(y):
+        numer = 1/alpha * (y - b)
+        if epsilon==2:
+            denom = jnp.sum((numer)**2)
+        else:
+            denom = 1.0
+        return a + 1.0/denom * B @ numer
+
+    return mixing_moebius_transform, unmixing_moebius_transform

tests/test_data_generators.py
@@ -3,7 +3,7 @@ import numpy as np
 from jax import jacfwd
 from scipy.stats import ortho_group
 
-from ima.ima.mixing_functions import build_moebius_transform
+from ima_vae.data.data_generators import build_moebius_transform
 
 
 def test_moebius_orthogonality(args):