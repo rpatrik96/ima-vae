14f48120096295bbc9844991c11efc582c786880
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2022-03-11 14:23:58 +0100

    sweep job 200 min

--- scripts/wandb_sweep.sh
+++ scripts/wandb_sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 PARTITION=gpu-2080ti-preemptable
 #FLAGS= 
-PYTHONPATH=. srun --time=01:00:00 --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=4 --mem=8G --gpus=1 -- /mnt/qb/work/bethge/preizinger/ima-vae//scripts/run_singularity_server.sh wandb agent --count 1 "$@"
+PYTHONPATH=. srun --time=200 --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=4 --mem=8G --gpus=1 -- /mnt/qb/work/bethge/preizinger/ima-vae//scripts/run_singularity_server.sh wandb agent --count 1 "$@"
 
