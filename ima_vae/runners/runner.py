c8a7d88c61b37b2a7b4fb0bd95d5d26335f400a2
Patrik Reizinger <prmedia17@gmail.com>
2022-05-09 06:38:38 +0200

    cima logged/ all val epoch

--- ima_vae/runners/runner.py
+++ ima_vae/runners/runner.py
@@ -201,11 +201,11 @@ class IMAModule(pl.LightningModule):
 
         # self.val_mcc.update(sources=sources,estimated_factors=latent)
 
+        self._log_cima(latent, panel_name)
         if (
             self.current_epoch % 20 == 0
             or self.current_epoch == (self.trainer.max_epochs - 1)
         ) is True:
-            self._log_cima(latent, panel_name)
 
             self._log_amari_dist(obs, sources, panel_name)
             # self._log_true_data_likelihood(obs, panel_name) #uses jax
